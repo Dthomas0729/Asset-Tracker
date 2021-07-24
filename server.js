ed1e0af070a7050323b22ef70a33161a175ea626
Dominique Thomas <63202540+Dthomas0729@users.noreply.github.com>
2021-07-24 15:56:11 -0400

    ADD user & asset VIEWS folders, ADD passport.js, UPDATE server.js

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
 const express = require('express');
+const morgan = require('morgan');
+const session = require("express-session");
+const passport = require("passport");
+
+require('dotenv').config()
+require('./auth/passport');
+
 const PORT = 4000
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(
+    session({
+      secret: "SEIRFLEXRocks!",
+      resave: false,
+      saveUninitialized: true,
+    })
+  );
 
 app.listen(PORT, (err) => {
     console.log(`We Are Live! Go Here ---> http://localhost:${PORT}`);
