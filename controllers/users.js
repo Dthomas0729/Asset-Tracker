f8a473f8d566b32ca1fd429317f60d541b3475a8
Dominique Thomas <63202540+Dthomas0729@users.noreply.github.com>
2021-08-03 20:16:37 -0400

    add user.cashFlow to home page

--- controllers/users.js
+++ controllers/users.js
@@ -1,13 +1,46 @@
 const User = require('../models/user')
 const Asset = require('../models/asset')
 
+
+// function index(req, res) {
+//     User.find(req.user)
+//         .populate("assets")
+//         .exec((err, user) => {
+//             Asset.find({
+//                 user:  req.user.id
+//             }, (err, assets) => {
+//                 res.render('users/index', {user: req.user, assets})
+//             })
+//         })
+// }
+
 function index(req, res) {
-  User.find(req.user)
+    User.find(req.user)
         .populate("assets")
         .exec((err, user) => {
-            Asset.find({user: req.user._id}, function (err, assets) {
+            Asset.find({
+                user:  req.user.id
+            }, (err, assets) => {
+                let totalincome = 0;
+                let totalexpenses = 0;
+
+                assets.forEach((a) => {
+                    if (a.type === 'Stock') {
+                        return
+                    } else {
+                        totalincome += a.income;
+                        totalexpenses += a.expenses;
+                    }
+                })
+
+                User.findByIdAndUpdate(req.user.id, {
+                    cashFlow: totalincome-totalexpenses,
+
+                }, () => {
+                    console.log(req.user);            
+                })
+
                 res.render('users/index', {user: req.user, assets});
-                
             })
         })
 }
